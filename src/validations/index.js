d4864c131b774e9b862bb00adf70395de458a585
octaviofitz <octaviofitz@gmail.com>
2022-08-01 00:47:36 -0300

    modificaciones parciales index

---
+++ src/validations/index.js
@@ -0,0 +1,54 @@
+const { check } = require("express-validator");
+
+module.exports = [
+
+    /* Nombre */
+    check("nombre")
+    .notEmpty()
+    .withMessage("Debe ingresar su nombre")
+    .bail()
+    .isLength({
+      min: 3,
+      max: 30,
+    })
+    .withMessage("El nombre debe tener un mínimo de 3 caracteres"),
+
+    /* Correo */
+
+  check("email")
+    .notEmpty()
+    .withMessage("Debe ingresar un correo")
+    .isEmail()
+    .withMessage("Debe ingresar un correo valido"),
+
+    /* Número de contacto */
+
+    check("telefono")
+    .notEmpty()
+    .withMessage("Indique un número de contacto")
+    .bail()
+    .isLength({ max: 30 })
+    .withMessage("El maximo de caracteres permitidos es de 30")
+    .bail()
+    .isNumeric()
+    .withMessage("Solo se permiten números")
+    .bail(),
+
+    /* Asunto */
+
+    check("asunto")
+    .notEmpty()
+    .withMessage("Debe indicar un asunto")
+    .bail()
+    .isLength({ max: 100 })
+    .withMessage("El maximo de caracteres permitidos es de 100")
+    .bail(),
+
+    /* Mensaje */
+
+     check("mensaje")
+    .isLength({
+      max: 1000,
+    })
+    .withMessage("El numero máximo de caracteres es de 1000"),
+];
